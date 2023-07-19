778a0798f279408718a9a28f858ab2beafb514ae
Will Riffe <w_riffe@protonmail.com>
2023-07-18 21:21:33 -0400

    🔨 Constructed the Sign-up API handling logic

---
+++ public/js/sign-up/signupApi.js
@@ -0,0 +1,56 @@
+
+
+// Registers the user to the API endpoint
+async function registerUser(name, email, password) {
+    try {
+
+      // Make the API call to register the user
+      const response = await fetch("/api/user/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        // Convert user data to JSON
+        body: JSON.stringify({ 
+            name: name, 
+            email: email, 
+            password: password 
+        }), 
+        });
+  
+
+      // Parse the response as JSON
+      const data = await response.json();
+  
+    // Check if the email is already in use
+    if (data.message === "Issue with credentials") {
+        // Display an error alert if the email is not unique
+        showErrorAlert("Issue with credentials");
+        return;
+      }
+      
+      
+      // Show success alert if registration is successful
+      showSuccessAlert("User Registered!");
+  
+
+      // Redirect to a success page
+      window.document.location.href = "/sign-up/success";
+    } catch (err) {
+
+      // Log any errors that occurred during the API call
+      console.error(err);
+
+      // Show an error message to the user
+      showErrorAlert("Oops! Something went wrong. Please try again later.");
+    }
+  }
+
+  // Display error alert
+  function showErrorAlert(message) {
+    alert(message);
+  }
+  
+  // Display success alert
+  function showSuccessAlert(message) {
+    alert(message);
+  }
+  
