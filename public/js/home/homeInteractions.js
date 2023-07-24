fa5e77a712a7d87357c65a90f237b4613c4187a7
Will Riffe <w_riffe@protonmail.com>
2023-07-23 20:35:59 -0400

    ✨Improved appearance of the site

--- public/js/home/homeInteractions.js
+++ public/js/home/homeInteractions.js
@@ -1,72 +1,54 @@
-
-
-
 // Blog Post Container
-const postArticle = 
-    document.querySelector(
-        ".home-container"
-);
+const postArticles = document.querySelectorAll(".home-container");
 
 // Blog-Post Comment Container
-const commentSection = 
-    document.querySelector(
-        ".comment-section"
-);
+const commentSections = document.querySelectorAll(".comment-section");
 
-
-
-// All posts 
-const posts = document.querySelectorAll(".public-post");
-
-
-
-// Check if Null 
-if (postArticle) {
+// Check if Null
+if (postArticles) {
+  postArticles.forEach((postArticle) => {
     postArticle.addEventListener("click", async (e) => {
-    removePicked();
-    const post = e.target;
-    const welcomeUser = document.querySelector(".welcome-user");
+      removePicked();
+      const post = e.target;
+      const welcomeUser = document.querySelector(".welcome-user");
 
-    if (!welcomeUser) {
-      const confirmed = window.confirm("Sign in first or get lost!");
-      
-      if (confirmed) {
-        window.document.location.href = "/login";
-      } else {
-        return;
+      if (!welcomeUser) {
+        const confirmed = window.confirm("Sign in first or get lost!");
+
+        if (confirmed) {
+          window.document.location.href = "/login";
+        } else {
+          return;
+        }
       }
-    }
 
-    if (e.target.classList.contains("public-post")) {
-      const postId = post.getAttribute("value");
+      if (e.target.classList.contains("public-post")) {
+        const postId = post.getAttribute("value");
 
-      // Stashes home view, displays individual post
-      postArticle.style.display = "none";
-      commentSection.style.display = "none";
-      document.querySelector(".post-page").style.display = "block";
+        // Stashes home view, displays individual post
+        postArticles.forEach((article) => {
+          article.style.display = "none";
+        });
+        commentSections.forEach((section) => {
+          section.style.display = "none";
+        });
+        document.querySelector(".post-page").style.display = "block";
 
-      // Updates the displayed posts' values
-      const updatePostValues = document.querySelector(".displayed-post");
-      
-      updatePostValues.querySelector(".post-title")
-        .textContent = post.querySelector(".post-title").textContent;
+        // Updates the displayed posts' values
+        const updatePostValues = document.querySelector(".displayed-post");
 
-      updatePostValues.querySelector(".post-content")
-        .textContent = post.querySelector(".post-content").textContent;
+        updatePostValues.querySelector(".post-title").textContent = post.querySelector(".post-title").textContent;
+        updatePostValues.querySelector(".post-content").textContent = post.querySelector(".post-content").textContent;
+        updatePostValues.querySelector(".post-user").textContent = post.querySelector(".post-user").textContent;
+        updatePostValues.querySelector(".post-date").textContent = post.querySelector(".post-date").textContent;
 
-      updatePostValues.querySelector(".post-user")
-        .textContent = post.querySelector(".post-user").textContent;
+        // Update the link for viewing the full post
+        const displayPostLink = document.getElementById("fullPostPage");
+        displayPostLink.href = `/dashboard/post/${postId}`;
 
-      updatePostValues.querySelector(".post-date")
-        .textContent = post.querySelector(".post-date").textContent;
-      
-      // Update the link for viewing the full post
-      const displayPostLink = document.getElementById("fullPostPage");
-
-            displayPostLink.href = `/dashboard/post/${postId}`;
-
-      // Fetch comments for the picked post
-      getComments(postId);
-    }
+        // Fetch comments for the picked post
+        getComments(postId);
+      }
+    });
   });
 }
